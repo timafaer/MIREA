ae2411b1adfd124e1593292f8a20414ffadd587c
timafaer <timafaer@gmail.com>
2022-03-22 22:50:46 +0300

    add vector

--- MIREA/Simple.h
+++ MIREA/Simple.h
@@ -14,11 +14,11 @@ public:
 	}
 
 	void resize(int len) {
-		if (len >= range) {
+		if (len > range) {
 			range = len * 2;
 			T* tmp = elements;
 			elements = new T[range];
-			for (int i = 0; i < nlen; i++) 
+			for (int i = 0; i < nlen; i++)
 				elements[i] = tmp[i];
 			nlen = len;
 		}
@@ -27,5 +27,19 @@ public:
 		}
 	}
 
-};
+	T& operator[](int n) {
+		if (n < 0 && n >= nlen)
+			throw;
+		return elements[n];
+	}
 
+	void push(T el) {
+		if (range < nlen + 1)
+			resize(nlen + 1);
+		else
+			nlen++;
+		elements[nlen - 1] = el;
+	}
+
+	int size() { return nlen; }
+};
